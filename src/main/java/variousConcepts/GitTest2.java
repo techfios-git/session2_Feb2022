fe74f747bc41cbd8b11afe6b4b39fe86616149e6
shihabamin <amin.khandaker@gmail.com>
2022-06-26 10:14:58 -0500

    2nd commit

---
+++ src/main/java/variousConcepts/GitTest2.java
@@ -0,0 +1,14 @@
+package variousConcepts;
+
+public class GitTest2 {
+	
+	public static void main(String[] args) {
+		System.out.println("git test");
+	}
+	
+	public void printMe() {
+		System.out.println("git test 1");
+	}
+
+
+}
